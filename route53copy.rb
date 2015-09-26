69bc5cac6c8287a27abf216c9fc25f5a01b816cf
Anders Janmyr <anders@janmyr.com>
2015-09-26 16:17:58 +0200

    Added route53copy formula

---
+++ route53copy.rb
@@ -0,0 +1,18 @@
+class Goose < Formula
+  homepage "https://github.com/andersjanmyr/route53copy"
+  url "https://github.com/andersjanmyr/route53copy/archive/v1.1.1.tar.gz"
+  sha256 "1bd8bc7c4aad2a9d71c53cd17368da06b9b315d8577d9cba39bccd2b72b7f1e3"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    system "./build"
+    bin.install "bin/route53copy"
+  end
+
+  test do
+    system "#{bin}/route53copy", "--version"
+  end
+end
+
