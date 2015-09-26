f5771cdb26dfe35daa269ff0c54400d8f5a9c0a5
Anders Janmyr <anders@janmyr.com>
2015-09-26 16:19:31 +0200

    Changed name to Route53

--- route53copy.rb
+++ route53copy.rb
@@ -1,4 +1,4 @@
-class Goose < Formula
+class Route53copy < Formula
   homepage "https://github.com/andersjanmyr/route53copy"
   url "https://github.com/andersjanmyr/route53copy/archive/v1.1.1.tar.gz"
   sha256 "1bd8bc7c4aad2a9d71c53cd17368da06b9b315d8577d9cba39bccd2b72b7f1e3"
