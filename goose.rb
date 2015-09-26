a737c6959e0dd832dee6a9387c3fbc501b98ba93
Anders Janmyr <anders@janmyr.com>
2015-09-26 16:04:15 +0200

    Updated goose to 1.3.4

--- goose.rb
+++ goose.rb
@@ -1,7 +1,7 @@
 class Goose < Formula
   homepage "https://github.com/andersjanmyr/goose"
-  url "https://github.com/andersjanmyr/goose/archive/v1.3.2.tar.gz"
-  sha256 "a262722d00992163c36066d69734f284212d559bc9843a4d0496469228fa723f"
+  url "https://github.com/andersjanmyr/goose/archive/v1.3.4.tar.gz"
+  sha256 "4d3ee7f9f31083544a508514d74e60ec86877e88277b6dfe826bcd302f3008ce"
 
   depends_on "go" => :build
 
